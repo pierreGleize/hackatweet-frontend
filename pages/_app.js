522367b32b80e7b77ab4e6074e74407713b2cb17
Pierre Gleize <gleizepierre3gmail.com>
2024-11-22 16:19:08 +0100

    pierre

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,34 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import user from "../reducers/user";
 import likes from "../reducers/likes";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
 
+// const store = configureStore({
+//   reducer: { user, likes },
+// });
+
+const reducers = combineReducers({ user, likes });
+
+const persistConfig = { key: "hackatweet", storage };
 const store = configureStore({
-  reducer: { user, likes },
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Head>
-        <title>Hackatweet</title>
-      </Head>
-      <Component {...pageProps} />
+      <PersistGate persistor={persistor}>
+        <Head>
+          <title>Hackatweet</title>
+        </Head>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
